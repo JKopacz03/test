15bf4177eec0de2bc00a9a3b214b9d7048acadfa
Jarosław Kopacz <ignacyikopacz@gmail.com>
2024-02-27 00:18:17 +0100

    all

--- Consumer-Publisher/src/Server.java
+++ Consumer-Publisher/src/Server.java
@@ -2,13 +2,17 @@ import exceptions.IllegalConnectionException;
 
 import java.io.*;
 import java.net.*;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(9998);
         BlockingQueue<Socket> consumers = new LinkedBlockingQueue<>();
+        Map<Socket, PrintWriter> outputMap = new ConcurrentHashMap<>();
 
         while (true) {
             Socket socket = serverSocket.accept();
@@ -21,15 +25,18 @@ public class Server {
                     new Thread(() -> {
                         try {
                             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
                             String receivedMess;
                             while ((receivedMess = reader.readLine()) != null) {
-                                while (consumers.isEmpty()) {
-                                    Thread.sleep(1000);
-                                }
+                                Socket newe = consumers.take();
+                                consumers.offer(newe);
                                 for (Socket consumerSocket : consumers) {
-                                        PrintWriter out = new PrintWriter(consumerSocket.getOutputStream(), true);
+                                    if (consumerSocket.isConnected()) {
+                                        PrintWriter out = outputMap.get(consumerSocket);
                                         out.println(receivedMess);
+                                    } else {
+                                        consumers.remove(consumerSocket);
+                                        outputMap.remove(consumerSocket);
+                                    }
                                 }
                             }
                         } catch (IOException | InterruptedException e) {
@@ -38,6 +45,7 @@ public class Server {
                     }).start();
                 } else if (message.equals("CONSUMER_CONNECTION")) {
                     System.out.println("Consumer connected from port: " + socket.getPort());
+                    outputMap.put(socket, new PrintWriter(socket.getOutputStream(), true));
                     consumers.offer(socket);
                 } else {
                     throw new IllegalConnectionException("Unknown client connected from port: " + socket.getPort());
--- test.java
+++ test.java
@@ -5,7 +5,7 @@ public class Main {
                 "int i = 5;" +
                 " ttfyhthrrh11112--7878";
         String xd = " + int e = 0; + ";
-
+        String ee = " String i = "e";";
 //        String name2 = "Ale ma kot int i = 0; tak";
 
 //        String e1 = "d"; int iw = 0; String e2 = "x";
@@ -19,9 +19,13 @@ public class Main {
             int i = 1;
             String name = "xd";
          */
-        String namet = "xd";
-        /** int xd = 1; */
+        String in = 'int e = 0';
+        /**
+         *int xd = 1;
+         */
         int it = 0;
+
+
         /*
         int e = 0;
         String mentzenKrol = "pis i po jedno zlo";
